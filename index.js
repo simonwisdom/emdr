23c0d9608084c0f9ee694683c489fc0155601108
Simon Wisdom <swisdom@squareup.com>
2022-01-28 21:07:05 +1100

    Fixed some full height behaviour bugs Still a few left

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
 var ballRadius = 20;
 var x = canvas.width/2;
-var y = canvas.height/2;
+var y = 50;
 var dx = 0;
 var dy = 0;
 var ballColor = "#0095DD";
@@ -10,6 +11,7 @@ var storedDx = 60;
 var storedDy = 0.5;
 var widthShrink = 0;
 var yBounce = 3;
+var widthShrinkMultiplier = 300;
 
 document.getElementById("displayDx").innerHTML = storedDx^2;
 document.getElementById("displayYBounce").innerHTML = yBounce;
@@ -22,9 +24,8 @@ function drawBall() {
     ctx.closePath();
 }
 
-var time;
-
 (function draw() {
+
     setTimeout(function() {
 
         // ReDraw background rectance
@@ -36,6 +37,8 @@ var time;
         // Bounce off left and right border
         if(x + dx > canvas.width-ballRadius-widthShrink
             || x + dx < ballRadius + widthShrink) {
+            console.log('widthShrink',widthShrink);
+            console.log('Hit x border','x',x,'dx',dx)
             dx = -dx;
             // Bounce at a random shallow Y direction
             dy = (dy + yBounce) * (1 - 2 * Math.random());
@@ -43,6 +46,7 @@ var time;
 
         // Bounce off top and bottom border
         if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+            console.log('Hit y border','y',y,'dy',y)
             dy = -dy;
         }
 
@@ -51,7 +55,7 @@ var time;
         y += dy;
 
         // change the left and right inner boundaries
-        widthShrink = 300 * Math.random() * Math.random();
+        widthShrink = widthShrinkMultiplier * Math.random() * Math.random();
 
         // loop the draw function
         window.requestAnimationFrame(draw);
@@ -92,6 +96,11 @@ function adjustYbounce(bounce){
     // document.getElementById("displayDy").innerHTML = storedDy;
 }
 
+// Stopwatch to time the length of each session
+var seconds = 0;
+var Interval;
+var appendSeconds = document.getElementById("seconds")
+
 function startTimer () {
     seconds++;
     appendSeconds.innerHTML = seconds/10;
@@ -107,26 +116,30 @@ function clearTimer() {
     seconds = 0;
 }
 
-var seconds = 0;
-var Interval;
-var appendSeconds = document.getElementById("seconds")
+function resetPosition(){
+    widthShrinkMultiplier = 0;
+    dx = 0;
+    dy = 0;
+    x = canvas.width/4;
+    y = 10 + ballRadius;
+}
 
 // Freeze the ball on button click, or restart movement
 function pause(){
     if (Math.abs(dx) > 0 || Math.abs(dy) > 0){
-        widthShrink = 0;
-        dx = 0;
-        dy = 0;
-        x = canvas.width/2;
+        resetPosition();
         clearTimer();
         } else {
           dx = storedDx;
           dy = storedDy;
           startSession();
+          widthShrinkMultiplier = 300;
         }
 }
 
 // Fullscren when running
+var elem = document.body; // Make the body go full screen.
+
 function requestFullScreen(element) {
     // Supports most browsers and their versions.
     var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
@@ -141,22 +154,32 @@ function requestFullScreen(element) {
     }
 }
 
-var elem = document.body; // Make the body go full screen.
-
 
 // Hide buttons when unpaused
 const targetDiv = document.getElementById("buttons");
 var canvasHeight = canvas.height;
+var canvasHeightFull = window.innerHeight - 50
+
+function resizeCanvas() {
+
+    // resetPosition();
+    canvas.height = canvasHeight;
+    canvas.width = window.innerWidth;
+}
+window.addEventListener('resize', resizeCanvas, false);
 
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
         if (targetDiv.style.display !== "none") {
             targetDiv.style.display = "none";
             pause();
-            canvas.height = window.innerHeight
+            // Full height
+            canvas.height = canvasHeightFull;
+            canvas.width = window.innerWidth;
           } else {
             targetDiv.style.display = "block";
             pause();
+            // Original height
             canvas.height = canvasHeight
           }
     }
@@ -174,6 +197,10 @@ function toggleTimer(){
 function toggleFullscreen() {
     if (document.fullscreenElement) {
         document.exitFullscreen()
+        console.log('x',x,'y',y)
+        resizeCanvas();
+        // Original height
+        // canvas.height = canvasHeight
       } else {
         document.documentElement.requestFullscreen();
     }
@@ -186,4 +213,3 @@ document.addEventListener("focus", (e) => {
   }, true);
 
 
-// Stopwatch to time the length of each session
